598dee9fd52ff9b53f1844f03e36f91e50634c94
MedvedevWeb <gy7swawf>
2016-03-31 20:27:50 +0300

    initial commit

---
+++ handlers/send-mail.php
@@ -0,0 +1,73 @@
+<?php
+
+include 'MailChimp.php';
+
+use \DrewM\MailChimp\MailChimp;
+
+$mainEmail = 'wildwind.flamin@gmail.com';
+
+if (!$_POST) exit();
+
+$dc = 'UTF-8';
+$sc = 'windows-1251';
+$name = 'Request';
+
+$header = array(
+	'name'      => mime_header_encode($name, $dc, $sc),
+	'emailFrom' => mime_header_encode($mainEmail, $dc, $sc),
+	'email'     => mime_header_encode($mainEmail, $dc, $sc)
+);
+
+if($_POST['action'] == 'request'){
+	$subject = 'Новая заявка';
+
+	$mailchimpData = array(
+		'email' => filter_var($_POST['contacts'], FILTER_VALIDATE_EMAIL)
+	);
+
+	$interest = $_POST['interest'] ? implode(', ', $_POST['interest']) : 'Не указано';
+	$beginner = $_POST['beginner'] ? 'Да' : 'Нет';
+
+	$message = <<<__MSG
+		<b>Новичок:</b> {$beginner}<br>
+		<b>Интересует:</b> {$interest}<br>
+		<b>Имя:</b> {$_POST['name']}<br>
+		<b>Контакты:</b> {$_POST['contacts']}<br>
+		<b>Вопрос:</b> {$_POST['question']}<br>
+__MSG;
+} elseif ($_POST['action'] == 'subscribe'){
+	$mailchimpData = array(
+		'email' => filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)
+	);
+
+	$subject = 'Новый подписчик';
+	$message = "Email: {$_POST['email']}";
+}
+
+$headers  = ''
+.	"Content-type: text/html; charset=windows-1251 \r\n"
+.	"From: SlideAway <{$header['emailFrom']}>";
+
+mail($mainEmail, $subject, iconv($dc, "{$sc}//IGNORE", $message), $headers);
+
+if($mailchimpData['email']){
+	$params = array(
+		'apiKey' => '241158e6e1de81f12509d927c5f21d91-us13',
+		'listId' => 'ada6ad7261'
+	);
+
+	$MailChimp = new MailChimp($params['apiKey']);
+
+	$result = $MailChimp->post("lists/{$params['listId']}/members", array(
+        'email_address' => $mailchimpData['email'],
+        'status'        => 'subscribed'
+    ));
+}
+
+echo 'Success.';
+
+function mime_header_encode($str, $data_charset, $send_charset) {
+	$str = iconv($data_charset, "{$send_charset}//IGNORE", $str);
+
+	return ("=?{$send_charset}?B?" . base64_encode($str) . '?=');
+}
---
+++ js/scripts.js
@@ -0,0 +1,110 @@
+$(function() {
+    var isMobile = is_touch_device() || $(window).width() <= 1024;
+
+    if(isMobile) $('.body').addClass('mobile');
+
+    $(window).load(function(){
+        if(!isMobile) initSlides();
+    });
+
+    $('input[type="text"]').on('blur', inputPlaceholder);
+    $('#request, #subscribe').on('submit', formSubmit);
+});
+
+function is_touch_device() {
+  return !!('ontouchstart' in window);
+}
+
+function inputPlaceholder()
+{
+    var self = $(this);
+
+    if($(this).val()){
+        self.addClass('not-empty');
+    } else {
+        self.removeClass('not-empty');
+    }
+}
+
+function formSubmit( e )
+{
+    e.preventDefault();
+
+    var form = $(this);
+    var action = form.attr('id');
+    var requireInput = {};
+
+    var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+
+    switch(action){
+        case 'request':
+            requireInput = form.find('input[name="contacts"]');
+            if(!requireInput.val()){
+                requireInput.addClass('error');
+                return false;
+            }
+            break;
+        case 'subscribe':
+            requireInput = form.find('input[name="email"]');
+            if(!pattern.test(requireInput.val())){
+                requireInput.addClass('error');
+                return false;
+            }
+            break;
+    }
+
+    requireInput.removeClass('error');
+
+    $.ajax({
+        type: 'POST',
+        url: '/handlers/send-mail.php',
+        dataType: 'json',
+        data: 'action=' + action + '&' + form.serialize(),
+        complete: function(data) { form.addClass('sended'); }
+    });
+
+    return false;
+}
+
+function initSlides()
+{
+    var slides = $('.slide');
+    var current = 0;
+    var total = slides.length;
+    var swipeAllowed = true;
+
+    slides.on('mousewheel', switchSlide);
+    $('input[type="text"]').on('focus', inputFocus);
+
+    function inputFocus()
+    {
+        var self = $(this);
+        var slide = self.closest('.slide');
+
+        if(!slide.hasClass('current')){
+            $('.slide').removeClass('current');
+            slide.addClass('current');
+
+            current = slide.index();
+        }
+    }
+
+    function switchSlide( e )
+    {
+        if(!swipeAllowed) return false;
+        swipeAllowed = false;
+        setTimeout(function(){ swipeAllowed = true; }, 1500);
+
+        var next = current - (e.deltaY < 0 ? -1 : 1);
+
+        if(next >= 0 && next < total){
+            if(next != 0){
+                $('video')[0].pause();
+            } else {
+                $('video')[0].play();
+            }
+            slides.removeClass('current').eq(next).addClass('current');
+            current = next;
+        }
+    }
+}
